c9cb9811e93e726cb6242142d48cc5fe1521213f
Suman Saurabh Das <dsumansaurabh@gmail.com>
2022-09-30 10:31:42 +0530

    Tutorial 18 (List and Keys) - Completed

--- src/components/section2/NameList.js
+++ src/components/section2/NameList.js
@@ -2,8 +2,16 @@ import React from 'react'
 import Person from './Person'
 /*
     Rendering a simple array of items
-    Rendering an array of object with mutiple properties
+    Rendering an array of object with multiple properties
         For this the best practice is to create a component which is only responsible for displaying the data and sending the data as props from the parent component (separation of concerns)
+
+    In console we get a warning saying that "Each child in a list should have a unique "key" prop"
+    In simpler words, each item in the list rendered using map should have a prop called key and the value to that prop should be unique in the list
+    NOTE - Key prop need not be an id always, it can be anything as long as it is unique
+    NOTE - Key props are not accessible in the child components. We will get this warning - "Person: `key` is not a prop. Trying to access it will result in `undefined` being returned."
+
+    Importance of key -
+    Keys help react identify which items in the list have changed, added or removed and plays a crucial role in handling UI updates efficiently.
 */
 
 function NameList() {
@@ -29,7 +37,7 @@ function NameList() {
         </>
     )
     */
-
+   
     const persons = [
         {
             id: 1,
@@ -50,7 +58,7 @@ function NameList() {
             skill: "Vue"
         }
     ]
-    const personList = persons.map(persons => <Person persons={persons}/>)
+    const personList = persons.map(persons => <Person key={persons.id} persons={persons}/>)
     return(
         <>
             {personList}
--- src/components/section2/Person.js
+++ src/components/section2/Person.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 /*
-    The Person component is only responsible for rendering the data which it gets from the parent
+    The Person component is only responsible for rendering the data which it gets from the parent.
 */
 
-function Person({persons}) {
+function Person({persons, key}) {
     return (
         <>
             <h2>
-                I am {persons.name}. I am {persons.age} years old. I know {persons.skill}
+                I am {persons.name}. I am {persons.age} years old. I know {persons.skill} {key}
             </h2>
         </>
     )
